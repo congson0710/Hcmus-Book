cd3cc3494b439835f76466ba5c2f06f009cbcbf9
congson0710 <congson0710@gmail.com>
2018-04-23 15:50:44 +0700

    Routes config

--- client/src/App/index.jsx
+++ client/src/App/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './Header';
 import Footer from './Footer';
+import Home from './Home';
 
 const App = props => (
   <div>
     <Header />
-    <div>{props.children}</div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
     <Footer />
   </div>
 );
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-import Home from './App/Home';
 
 const Routes = () => (
-  <BrowserRouter>
-    <div>
-      <Route path="/" component={App} />
-    </div>
-  </BrowserRouter>
+  <Router>
+    <App />
+  </Router>
 );
 
 export default Routes;
