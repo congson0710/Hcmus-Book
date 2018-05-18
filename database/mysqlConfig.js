3450df8e6b22401e03b1073a08e2135c408dc43c
congson0710 <congson0710@gmail.com>
2018-05-18 23:54:24 +0700

    re-config connection with database

--- database/mysqlConfig.js
+++ database/mysqlConfig.js
@@ -1,34 +1,14 @@
 const Sequelize = require('sequelize');
 
-// const keys = require('../config/key');
+const keys = require('../config/key');
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = new Sequelize(
-    'gcp_33023d63a4b70199cc1d',
-    'b8f4920c05ed20',
-    '4312efa3',
-    {
-      host: 'us-cdbr-gcp-east-01.cleardb.net',
-      port: '3306',
-      dialect: 'mysql',
-      define: {
-        timestamps: false,
-        freezeTableName: true,
-      },
-      operatorsAliases: false,
-
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-    }
-  );
-} else {
-  module.exports = new Sequelize('hcmus-book', 'root', '123456', {
-    host: '127.0.0.1',
-    port: '8000',
+module.exports = new Sequelize(
+  keys.databaseName,
+  keys.databaseUser,
+  keys.databasePassword,
+  {
+    host: keys.databaseHost,
+    port: keys.databasePort,
     dialect: 'mysql',
     define: {
       timestamps: false,
@@ -42,5 +22,5 @@ if (process.env.NODE_ENV === 'production') {
       acquire: 30000,
       idle: 10000,
     },
-  });
-}
+  }
+);
