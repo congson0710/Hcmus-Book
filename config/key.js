2c85d2d195ea77b0582ed3f80555b568c0fd3d3c
congson0710 <congson0710@gmail.com>
2018-05-19 00:08:23 +0700

    update

--- config/key.js
+++ config/key.js
@@ -1,7 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
   // export production set of keys
+  console.log('environment', process.env.NODE_ENV);
   module.exports = require('./prod');
 } else {
   // export development set of keys
+  console.log('environment', process.env.NODE_ENV);
   module.exports = require('./dev');
 }
