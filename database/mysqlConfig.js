93ada9fa98afac3ebefa0e4fbbd773f9259b4744
congson0710 <congson0710@gmail.com>
2018-05-18 23:32:18 +0700

    update

--- database/mysqlConfig.js
+++ database/mysqlConfig.js
@@ -1,14 +1,34 @@
 const Sequelize = require('sequelize');
 
-const keys = require('../config/key');
+// const keys = require('../config/key');
 
-module.exports = new Sequelize(
-  keys.databaseName,
-  keys.databaseUser,
-  keys.databasePassword,
-  {
-    host: keys.databaseHost,
-    port: keys.databasePort,
+if (process.env.NODE_ENV === 'production') {
+  module.exports = new Sequelize(
+    'gcp_33023d63a4b70199cc1d',
+    'b8f4920c05ed20',
+    '4312efa3',
+    {
+      host: 'us-cdbr-gcp-east-01.cleardb.net',
+      port: '3306',
+      dialect: 'mysql',
+      define: {
+        timestamps: false,
+        freezeTableName: true,
+      },
+      operatorsAliases: false,
+
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    }
+  );
+} else {
+  module.exports = new Sequelize('hcmus-book', 'root', '123456', {
+    host: '127.0.0.1',
+    port: '8000',
     dialect: 'mysql',
     define: {
       timestamps: false,
@@ -22,5 +42,5 @@ module.exports = new Sequelize(
       acquire: 30000,
       idle: 10000,
     },
-  }
-);
+  });
+}
